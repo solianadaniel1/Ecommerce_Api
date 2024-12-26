e938b1ae28e187c660bd1b4d859a63ab0c50a56b
solianadaniel1 <solianadaniel22@gmail.com>
2024-12-26 09:43:41 +0300

    Added product app on admin panel

---
+++ product_api/product/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from .models.product import Product
+from .models.category import Category 
+from .models.review import Review 
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'stock_quantity','price', 'display_categories']  # Display categories in the list view
+    search_fields = ['name', 'stock_quantity', 'price', 'stock_quantity']
+    list_filter = ['categories',]  # Allow filtering by categories in the sidebar
+    readonly_fields = ['updated_date','created_date'] # Prevent manual editing
+
+    def display_categories(self, obj):
+        return ", ".join([category.name for category in obj.categories.all()])
+    display_categories.short_description = 'Categories'  # Column header in the admin list view
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','parent_category')  # Show category name in the list view
+    search_fields = ('name','parent_category')  # Add search functionality for categories
+    readonly_fields = ('updated_at','created_at')  # Prevent manual editing
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('product','rating', 'comment')  # Show review products in the list view
+    search_fields = ('product','rating', 'comment')  # Add search functionality for review
+    readonly_fields = ('updated_at','created_at')  # Prevent manual editing
+
+admin.site.register(Review, ReviewAdmin)
